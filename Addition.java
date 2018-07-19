155a213ee76659b8ff8bd539bde21b687a2d856f
NaveenAmra <amra.naveen@gmail.com>
2018-07-19 12:23:38 +0530

    addition file added

---
+++ Addition.java
@@ -0,0 +1,23 @@
+package javaprograms;
+
+public class Addition {
+
+		 void add()
+		{
+			int first = 10;
+			int second= 20;
+			int sumvalue;
+			
+		    sumvalue = first + second;
+
+		    System.out.println("Addition of two numbers is :" + sumvalue);
+
+		}
+		 
+		 
+		 public static void main(String args[]){
+			 Addition add=new Addition();
+			 add.add();
+					 
+		 }
+}
